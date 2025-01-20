e4453bacfb30823535d1ab28fe089673fb7c0157
sungmoo0608 <swingsm0608@gmail.com>
2025-01-20 15:14:41 +0900

    Javascript-learn3-01-variable

---
+++ 01_basics/03_declaring_variable.js
@@ -0,0 +1,51 @@
+'use strict';
+
+/**
+ * Variable 선언하기
+ *
+ * 1) var - 더이상 쓰지 않는다.
+ * 2) let
+ * 3) const
+ *
+ */
+
+var name = '코드공장';
+console.log(name);
+
+var age = 32;
+console.log(age);
+
+let ive = '아이브';
+console.log(ive);
+
+/**
+ *
+ * let과 var로 선연하면
+ * 값을 추후 변경할 수 있다.
+ *
+ */
+
+ive = '안유진';
+console.log(ive);
+
+const newJeans = '뉴진스';
+console.log(newJeans);
+
+// newJeans = '다비치';
+
+/**
+ *
+ * 선언과 할당
+ *
+ * 1) 변수를 선언하는 것.
+ * 2) 할당
+ *
+ */
+
+var name = '코드나라';
+console.log(name);
+
+let girlFriend;
+console.log(girlFriend);
+
+// const girlFriend2;
